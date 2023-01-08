6f707b587405f215076d2a3fe636fe52eb36538a
chung <chunghuihan9@gmail.com>
2023-01-08 23:26:53 +0900

    second commit

--- app/src/main/java/com/example/api_project/MainActivity.kt
+++ app/src/main/java/com/example/api_project/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.api_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.api_project.databinding.ActivityMainBinding
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private var mbinding  : ActivityMainBinding? = null
+    private val binding get() = mbinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener(){
+
+        }
     }
 }
